330975982e9e31f46a004242c71fb38eec31bc88
pavlo.zubrytski@gmail.com <pzubrytskyi@llnw.com>
2021-04-12 04:15:24 +0300

    Add testing production s3 terraform

---
+++ resources/production_s3/main.tf
@@ -0,0 +1,16 @@
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "sketch-production-s3"
+  create_bucket = true
+  acl = "public-read-write"
+  force_destroy = true
+}
+
+output "bucket_endpoint" {
+  value = module.s3-bucket.this_s3_bucket_bucket_domain_name
+}
+
+output "bucket_name" {
+  value = module.s3-bucket.this_s3_bucket_id
+}
